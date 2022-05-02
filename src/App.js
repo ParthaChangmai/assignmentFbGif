78005e33b36af1e66cccea418dda5f9c0dc0d15b
ParthaChangmai <partha.partha.changmai@gmail.com>
2022-05-03 03:30:22 +0530

    spelling changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import Feed from './container/Feed';
-import Newpost from './container/Newpost';
+import NewPost from './container/NewPost';
 
 function App() {
 	return (
 		<div className="flex flex-col items-center min-h-screen gap-4 p-4 lg:px-36 xl:px-96 ">
-			<Newpost />
+			<NewPost />
 			<Feed />
 		</div>
 	);
