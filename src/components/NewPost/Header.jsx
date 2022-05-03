1672016f9f445ecc2a320d0901020bb567bc4c6d
ParthaChangmai <partha.partha.changmai@gmail.com>
2022-05-04 04:50:30 +0530

    text color

--- src/components/NewPost/Header.jsx
+++ src/components/NewPost/Header.jsx
@@ -34,7 +34,7 @@ function Header() {
 							d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
 						/>
 					</svg>
-					<span className="border-r-2 border-grey-500 pr-5">
+					<span className="border-r-2 border-grey-500 pr-5 text-cyan-600">
 						Photos / Videos
 					</span>
 				</div>
@@ -54,7 +54,7 @@ function Header() {
 							d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z"
 						/>
 					</svg>
-					<span className=" pr-5">Live Video</span>
+					<span className=" pr-5 text-cyan-600">Live Video</span>
 				</div>
 			</div>
 		</>
