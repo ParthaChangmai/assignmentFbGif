76ea5c87c18df8ee716bfb91981dfce350da7eba
ParthaChangmai <partha.partha.changmai@gmail.com>
2022-05-03 22:58:21 +0530

    completed the feed ui

--- src/container/Feed.jsx
+++ src/container/Feed.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import Body from '../components/Feed/Body';
+import CommentDemo from '../components/Feed/CommentDemo';
+import { Comments } from '../components/Feed/Comments';
+import Header from '../components/Feed/Header';
+import Likes from '../components/Feed/Likes';
 
 const Feed = () => {
-	return <div>Feed</div>;
+	return (
+		<div className="flex flex-col w-[550px]  gap-2 border rounded-lg shadow-lg">
+			<Header />
+			<Body />
+			<Likes />
+			<Comments />
+			<CommentDemo />
+		</div>
+	);
 };
 
 export default Feed;
