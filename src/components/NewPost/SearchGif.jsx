211f5d98dd069a5d42cd05a9ee5e63b1145f3ac6
ParthaChangmai <partha.partha.changmai@gmail.com>
2022-05-04 00:20:55 +0530

    added search option

--- src/components/NewPost/SearchGif.jsx
+++ src/components/NewPost/SearchGif.jsx
@@ -4,13 +4,22 @@ import RenderGifs from './RenderGifs';
 
 const SearchGif = () => {
 	const [gifData, setGifData] = useState([]);
+	const [searchGif, setSearchGif] = useState('');
 
 	useEffect(() => {
-		getGif().then((data) => {
-			setGifData(data);
-		});
-	}, []);
+		if (searchGif === '') {
+			getGif().then((data) => {
+				setGifData(data);
+			});
+		} else {
+			getGif('search', { searchGif }).then((data) => {
+				setGifData(data);
+			});
+		}
+	}, [searchGif]);
+
 	console.log(gifData);
+	console.log(searchGif);
 	return (
 		<>
 			<div className="absolute flex flex-col gap-4 px-4 py-2 overflow-y-scroll bg-gray-200 rounded-md shadow-lg max-h-80 w-72  -left-2 top-9">
@@ -18,6 +27,8 @@ const SearchGif = () => {
 					type="text"
 					onClick={(e) => e.stopPropagation()}
 					className="w-full h-8 outline-none rounded-3xl"
+					value={searchGif}
+					onChange={(e) => setSearchGif(e.target.value)}
 				/>
 				<div className="flex flex-col gap-2">
 					{gifData.map((gif) => (
