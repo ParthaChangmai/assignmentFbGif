84f609a96a4e35e305d214839822cd8a6c7aa927
ParthaChangmai <partha.partha.changmai@gmail.com>
2022-05-04 04:22:02 +0530

    added single gif function

--- src/components/NewPost/SearchGif.jsx
+++ src/components/NewPost/SearchGif.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { getGif } from '../../api';
 
-const SearchGif = () => {
+const SearchGif = (props) => {
 	const [gifData, setGifData] = useState([]);
 	const [searchGif, setSearchGif] = useState('');
 	const [isLoading, setIsLoading] = useState(false);
@@ -14,22 +14,25 @@ const SearchGif = () => {
 				setIsLoading(false);
 			});
 		} else {
-			getGif('search', { searchGif }).then((data) => {
-				setGifData(data);
-				setIsLoading(false);
+			const timeout = setTimeout(() => {
+				getGif('search', { searchGif }).then((data) => {
+					setGifData(data);
+					setIsLoading(false);
+				}, 500);
+
+				return () => clearTimeout(timeout);
 			});
 		}
 	}, [searchGif]);
 
-	console.log(gifData);
-	console.log(searchGif);
 	return (
 		<>
 			<div className="absolute flex flex-col gap-4 px-4 py-2 overflow-y-scroll bg-gray-200 rounded-md shadow-lg max-h-80 w-72  -left-2 top-9">
 				<input
+					placeholder="Search GIFS"
 					type="text"
 					onClick={(e) => e.stopPropagation()}
-					className="w-full h-8 outline-none rounded-3xl"
+					className="w-full h-8 outline-none rounded-lg"
 					value={searchGif}
 					onChange={(e) => setSearchGif(e.target.value)}
 				/>
@@ -41,6 +44,9 @@ const SearchGif = () => {
 							src={gif.images.downsized.url}
 							key={gif.id}
 							alt={gif.title}
+							onClick={() =>
+								props.getSingleGif(gif.images.downsized.url, gif.title)
+							}
 						/>
 					))}
 				</div>
